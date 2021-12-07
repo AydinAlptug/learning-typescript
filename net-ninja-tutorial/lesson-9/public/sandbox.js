90f237dacb614dd60cab75ea5fefaa0bb8c51976
AydinAlptug <yusufaydinceng@gmail.com>
2021-12-08 00:30:13 +0300

    sandbox.js

--- net-ninja-tutorial/lesson-9/public/sandbox.js
+++ net-ninja-tutorial/lesson-9/public/sandbox.js
@@ -4,33 +4,18 @@
 // compile with tsc (or tsc --watch)
 // it will place the compiled js in outDir
 // to prevent the ts files outside "src" to be compiled, add "include" : ["src"] field in tsconfig.json
-var greet = function () {
-    console.log('Hello World!');
+var logDetails = function (uid, item) {
+    console.log(item + " has a uid of " + uid);
 };
-// greet = 'hello'; // error: greet is a function, not a string
-var greetIt;
-greetIt = function () {
-    console.log('Salut!');
+var greet = function (user) {
+    console.log(user.name + " says hello");
 };
-var add = function (a, b, c) {
-    console.log(a + b);
-    console.log(c); // undefined if c is not provided
+var greetAgain = function (user) {
+    console.log(user.name + " says hello again");
 };
-// add(1, '2'); // error: string is not assignable to number
-add(1, 2);
-add(1, 2, '3');
-var divide = function (a, b, c) {
-    if (c === void 0) { c = 10; }
-    console.log(a / b);
-    console.log(c); // 10 if c is not provided
+var greetAgainAgain = function (user) {
+    console.log(user.name + " says hello again again");
 };
-divide(1, 2);
-divide(1, 2, '3');
-var minus = function (a, b) {
-    return a - b;
-};
-var result = minus(3, 2); // typescript infers the return type as number
-// result = 'some string'; // error: string is not assignable to number
-var log_multiply = function (a, b) {
-    console.log(a * b);
+var betterGreet = function (user) {
+    console.log(user.name + " says hello");
 };
